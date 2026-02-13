7ab5cc3f719d63ba156d95d5626cbf8441a6b6b5
michael usidamen <m.usidamen@gmail.com>
2026-02-13 13:11:46 +0100

    fixes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,7 @@ export const Footer = () => {
                             </div>
                         </div>
                         <p className="text-white/70 mb-8 leading-relaxed">
-                            Empowering People Building the Future
+                            Empowering People, Building the Future
                         </p>
                         <div className="flex gap-4">
                             {[Facebook, Twitter, Linkedin, Instagram].map((Icon, i) => (
